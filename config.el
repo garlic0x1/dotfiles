0404d0f305abdcb2af6f94e50739396afed81424
garlic0x1 <43155857+garlic0x1@users.noreply.github.com>
2023-02-19 20:56:53 +0000

    Add files via upload

---
+++ config.el
@@ -0,0 +1,8 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+(setq user-full-name "garlic0x1"
+      user-mail-address "garlic1@protonmail.com")
+
+(setq doom-theme 'doom-old-hope)
+(setq display-line-numbers-type t)
+(setq org-directory "~/org/")
