928dc4acc8f73b6a2c05a4fd45943d875c04a84c
garlic0x1 <43155857+garlic0x1@users.noreply.github.com>
2023-09-22 03:38:50 -0500

    Add files via upload

--- config.scm
+++ config.scm
@@ -1,4 +1,4 @@
-;; -*- mode: scheme; -*-
+
 ;; This is an operating system configuration template
 ;; for a "desktop" setup without full-blown desktop
 ;; environments.
@@ -7,16 +7,59 @@
  (gnu)
  (gnu system nss)
  (gnu packages vim)
- (gnu packages networking)
- (gnu packages admin)
+ ;; (gnu packages gcc)
+ ;; (gnu packages haskell)
+ ;; (gnu packages lisp)
+ ;; (gnu packages networking)
+ ;; (gnu packages admin)
+ ;; (gnu packages version-control)
  ;; browser
  (gnu packages gnuzilla)
- (gnu packages fonts)
+ ;; (gnu packages fonts)
  ;; absolutely proprietary
  (nongnu packages linux)
  (nongnu system linux-initrd))
 (use-service-modules desktop)
-(use-package-modules bootloaders certs emacs emacs-xyz screen ssh xorg)
+(use-package-modules bootloaders
+                     gnome
+                     lisp
+                     haskell
+                     gcc
+                     vim
+                     networking
+                     admin
+                     version-control
+                     certs
+                     fonts
+                     screen
+                     ssh
+                     xorg
+                     wm
+		     rust-apps
+                     suckless)
+
+;; adjust brightness without sudo
+(define %backlight-udev-rule
+  (udev-rule
+   "90-backlight.rules"
+   (string-append "ACTION==\"add\", SUBSYSTEM==\"backlight\", "
+                  "RUN+=\"/run/current-system/profile/bin/chgrp video /sys/class/backlight/%k/brightness\""
+                  "\n"
+                  "ACTION==\"add\", SUBSYSTEM==\"backlight\", "
+                  "RUN+=\"/run/current-system/profile/bin/chmod g+w /sys/class/backlight/%k/brightness\"")))
+
+(define %nonguix/desktop-services
+  (modify-services
+   %desktop-services
+   (guix-service-type
+    config => (guix-configuration
+	       (inherit config)
+	       (substitute-urls
+		(append (list "https://substitutes.nonguix.org")
+			%default-substitute-urls))
+	       (authorized-keys
+		(append (list (local-file "/etc/signing-key.pub"))
+			%default-authorized-guix-keys))))))
 
 (operating-system
 
@@ -38,10 +81,6 @@
                   (mount-point "/boot/efi")
                   (type "vfat"))
                  (file-system
-                  (device "/dev/sda2")
-                  (mount-point "/var/swap"))
-                 (type "btrfs")
-                 (file-system
                   (device "/dev/sda3")
                   (mount-point "/")
                   (type "ext4")))
@@ -50,11 +89,7 @@
  ;; UEFI boot
  (bootloader (bootloader-configuration
               (bootloader grub-efi-bootloader)
-              (target "/boot/efi")))
-
- ;; swap partition
- (swap-space
-  (target "/var/swap"))
+              (targets '("/boot/efi"))))
 
  (users (cons (user-account
                (name "garlic")
@@ -64,18 +99,16 @@
               %base-user-accounts))
 
  (packages (append (list
-                    openssh git              ;; utils
-                    emacs emacs-evil vim     ;; editors
-		            iwd wpa-supplicant       ;; wifi tools
-                    xterm                    ;; terminal emulators
-                    icecat liberation-fonts  ;; browser
-                    nss-certs                ;; for HTTPS access
+                    openssh git gcc xterm ripgrep fd ;; utils
+                    iwd wpa-supplicant		     ;; wifi tools
+                    xmodmap sbcl stumpwm `(,stumpwm "lib")
+                    gnome
+                    vim	      ;; editors
+                    nss-certs ;; for HTTPS access
                     )
                    %base-packages))
 
- ;; Use the "desktop" services, which include the X11
- ;; log-in service, networking with NetworkManager, and more.
- (services %desktop-services)
+ (services %nonguix/desktop-services)
 
  ;; Allow resolution of '.localhttps://systemcrafters.cc/craft-your-system-with-guix/full-system-install/' host names with mDNS.
  (name-service-switch %mdns-host-lookup-nss))
