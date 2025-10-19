49cda5855f98ae00380b37cfca0b4ab3c1c59eb1
garlic0x1 <garlic1@protonmail.com>
2025-10-19 12:21:07 +0300

    nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,7 +1,9 @@
 -- init.lua
 
 -- Bootstrap lazy.nvim plugin manager
+local opt = vim.opt
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",
@@ -12,30 +14,110 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   })
 end
-vim.opt.rtp:prepend(lazypath)
+
+opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   {
     "miikanissi/modus-themes.nvim",
     priority = 1000,
     config = function()
-      -- You can customize the theme here
       require("modus-themes").setup({
- 	-- Uses system theme preference
         style = "auto",
- 	-- Options: "default", "tinted", "deuteranopia", "tritanopia"
-        variant = "tinted",
+        variant = "tinted", -- "default", "tinted", "deuteranopia", "tritanopia"
       })
-      
+
       vim.cmd("colorscheme modus")
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "master",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter").setup({
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = false },
+      })
+    end,
+  },
+
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          lua = { "stylua" },
+          python = { "black" },
+          javascript = { "prettier" },
+          typescript = { "prettier" },
+          json = { "prettier" },
+          yaml = { "prettier" },
+          markdown = { "prettier" },
+          html = { "prettier" },
+          css = { "prettier" },
+          go = { "gofmt" },
+        },
+        formatters = {
+          stylua = {
+            prepend_args = { "--indent-type", "Spaces", "--indent-width", "2" },
+          },
+        },
+        format_on_save = {
+          timeout_ms = 500,
+          lsp_fallback = true,
+        },
+      })
+    end,
+  },
+
+  {
+    "numToStr/Comment.nvim",
+    config = function()
+      require("Comment").setup()
+      vim.keymap.set("n", "<M-;>", "<Plug>(comment_toggle_linewise_current)", { desc = "Comment line" })
+      vim.keymap.set("v", "<M-;>", "<Plug>(comment_toggle_linewise_visual)", { desc = "Comment selection" })
+    end,
+  },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          mappings = {
+            i = {
+              ["<C-j>"] = "move_selection_next",
+              ["<C-k>"] = "move_selection_previous",
+            },
+          },
+        },
+      })
+      vim.keymap.set("n", "<C-x><C-f>", ":Telescope git_files<CR>", { silent = true, desc = "Find files" })
+      vim.keymap.set("n", "<C-x><C-g>", ":Telescope live_grep<CR>", { silent = true, desc = "Grep files" })
+      vim.keymap.set("n", "<C-x><C-b>", ":Telescope buffers<CR>", { silent = true, desc = "Browse buffers" })
     end,
   },
 })
 
--- Show line numbers
-vim.opt.number = false
--- Enable 24-bit RGB colors
-vim.opt.termguicolors = true
--- Sync with system clipboard
-vim.opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus"
+-- Clipboard
+opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus"
 
+-- Style
+opt.number = false
+opt.termguicolors = true
+opt.wrap = false
+opt.scrolloff = 4
+
+-- Search
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = false
+opt.incsearch = true
